8e7573590c40ec76818d084ffbc1359689818fc3
Piotr Petryka <ppwxp19@gmail.com>
2023-02-07 13:25:21 +0100

    adding list of vehicles

---
+++ src/main/java/InventoryOfVehicles.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class InventoryOfVehicles {
+
+
+
+    private ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+}
